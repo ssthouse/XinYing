faeaa43c066fed4ee1d1d183c5734552cd9c6a81
ssthouse <1194368995@qq.com>
2016-05-02 14:52:23 +0800

    😘

---
+++ app/src/main/java/ssthouse/love/xinying/MainActivity.java
@@ -0,0 +1,13 @@
+package ssthouse.love.xinying;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
