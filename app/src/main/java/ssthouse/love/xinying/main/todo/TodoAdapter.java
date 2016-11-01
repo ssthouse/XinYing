19105b490326b66d319aabf599a50629fcedd2f4
ssthouse <ssthouse@163.com>
2016-11-01 09:25:34 +0800

    Refresh .gitignore file

---
+++ app/src/main/java/ssthouse/love/xinying/main/todo/TodoAdapter.java
@@ -0,0 +1,8 @@
+package ssthouse.love.xinying.main.todo;
+
+/**
+ * Created by ssthouse on 30/10/2016.
+ */
+
+public class TodoAdapter {
+}
---
+++ app/src/main/java/ssthouse/love/xinying/main/todo/TodoModel.java
@@ -0,0 +1,9 @@
+package ssthouse.love.xinying.main.todo;
+
+/**
+ * 数据操作Model
+ * Created by ssthouse on 30/10/2016.
+ */
+
+public class TodoModel {
+}
