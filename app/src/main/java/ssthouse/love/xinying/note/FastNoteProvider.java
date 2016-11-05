413d3fdf0783cbcce21b4efded15f772c20d3a34
ssthouse <ssthouse@163.com>
2016-11-05 19:33:28 +0800

    Refactor fastnote WidgetProvider

---
+++ app/src/main/java/ssthouse/love/xinying/note/FastNoteProvider.java
@@ -0,0 +1,55 @@
+package ssthouse.love.xinying.note;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import ssthouse.love.xinying.R;
+
+/**
+ * Created by ssthouse on 2015/12/9.
+ */
+public class FastNoteProvider extends AppWidgetProvider {
+
+    /**
+     * @param context          上下文
+     * @param appWidgetManager widget manager
+     * @param appWidgetIds     TODO id?? 是什么
+     */
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        initWidget(context, appWidgetManager, appWidgetIds);
+    }
+
+    private void initWidget(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        RemoteViews rvs = new RemoteViews(context.getPackageName(), R.layout.widget_provider);
+        setRvClickListener(context, rvs);
+        setRvText(context, rvs);
+        appWidgetManager.updateAppWidget(appWidgetIds, rvs);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        ComponentName componentName = new ComponentName(context, FastNoteProvider.class);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_provider);
+        setRvText(context, remoteViews);
+        setRvClickListener(context, remoteViews);
+        AppWidgetManager.getInstance(context).updateAppWidget(componentName, remoteViews);
+    }
+
+    private void setRvClickListener(Context context, RemoteViews rvs) {
+        Intent intent = new Intent(context, FastNoteActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        rvs.setOnClickPendingIntent(R.id.id_ll_fast_note, pendingIntent);
+    }
+
+    private void setRvText(Context context, RemoteViews rvs) {
+        rvs.setTextViewText(R.id.id_tv, PreferenceHelper.getInstance(context).getNote());
+    }
+}
