d90042a000faa8d6bd5db223d8feab7b75e0b5eb
ssthouse <ssthouse@163.com>
2016-12-05 22:43:41 +0800

    Finish joke fragment ui holder

---
+++ app/src/main/java/ssthouse/love/xinying/joke/JokeFragment.java
@@ -0,0 +1,22 @@
+package ssthouse.love.xinying.joke;
+
+import ssthouse.love.xinying.R;
+import ssthouse.love.xinying.base.BaseFragment;
+
+/**
+ * Created by ssthouse on 05/12/2016.
+ */
+
+public class JokeFragment extends BaseFragment {
+
+
+    @Override
+    public int getContentView() {
+        return R.layout.fragment_joke;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
