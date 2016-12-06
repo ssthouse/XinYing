94e5794f064da2fe7d142271ce63c5b232458119
ssthouse <ssthouse@163.com>
2016-12-06 10:10:40 +0800

    Add joke listview from api

--- app/src/main/java/ssthouse/love/xinying/joke/JokeFragment.java
+++ app/src/main/java/ssthouse/love/xinying/joke/JokeFragment.java
@@ -1,7 +1,24 @@
 package ssthouse.love.xinying.joke;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import ssthouse.love.xinying.R;
 import ssthouse.love.xinying.base.BaseFragment;
+import ssthouse.love.xinying.joke.bean.JokeBean;
+import ssthouse.love.xinying.utils.ToastUtil;
+import timber.log.Timber;
 
 /**
  * Created by ssthouse on 05/12/2016.
@@ -9,6 +26,13 @@ import ssthouse.love.xinying.base.BaseFragment;
 
 public class JokeFragment extends BaseFragment {
 
+    @Bind(R.id.id_lv_jokes)
+    ListView lvJokes;
+
+    private List<JokeBean> jokeBeanList = new ArrayList<>();
+
+    public JokeFragment() {
+    }
 
     @Override
     public int getContentView() {
@@ -17,6 +41,64 @@ public class JokeFragment extends BaseFragment {
 
     @Override
     public void init() {
+        lvJokes.setAdapter(jokeAdapter);
 
+        //TODO: should save today's joke in disk to save network
+        JokerGenerator.getJokeList(new Callback<List<JokeBean>>() {
+            @Override
+            public void onResponse(Call<List<JokeBean>> call, Response<List<JokeBean>> response) {
+                for (JokeBean jokeBean : response.body()) {
+                    jokeBean.setContent(jokeBean.getContent().replace("<br/><br/>", "\n"));
+                }
+                jokeBeanList.addAll(response.body());
+                jokeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<JokeBean>> call, Throwable t) {
+                Timber.e("error");
+                ToastUtil.show(getContext(), "something is wrong: not network or something else\nplease tell ssthouse");
+            }
+        });
+    }
+
+    private BaseAdapter jokeAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return jokeBeanList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return jokeBeanList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_joke, parent, false);
+                viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.id_tv_joke_title);
+                viewHolder.tvContent = (TextView) convertView.findViewById(R.id.id_tv_joke_content);
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            //set value
+            viewHolder.tvTitle.setText(jokeBeanList.get(position).getTitle());
+            viewHolder.tvContent.setText(jokeBeanList.get(position).getContent());
+            return convertView;
+        }
+    };
+
+    private static class ViewHolder{
+        TextView tvTitle;
+        TextView tvContent;
     }
 }
---
+++ app/src/main/java/ssthouse/love/xinying/joke/JokeService.java
@@ -0,0 +1,19 @@
+package ssthouse.love.xinying.joke;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import ssthouse.love.xinying.joke.bean.JokeBean;
+
+/**
+ * Created by ssthouse on 06/12/2016.
+ */
+
+public interface JokeService {
+
+    public static final String BASIC_URL = "http://api.laifudao.com/";
+
+    @GET("/open/xiaohua.json")
+    Call<List<JokeBean>> getJokeLIst();
+}
