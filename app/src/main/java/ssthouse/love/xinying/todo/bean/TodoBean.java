93f0c1dd949aa0a62920be747f39c8fa953801da
ssthouse <ssthouse@163.com>
2016-12-18 16:42:04 +0800

    Finish date handler logic

--- app/src/main/java/ssthouse/love/xinying/todo/bean/TodoBean.java
+++ app/src/main/java/ssthouse/love/xinying/todo/bean/TodoBean.java
@@ -4,13 +4,14 @@ import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Created by ssthouse on 16/9/3.
  */
 @Table(name = "Todo")
-public class TodoBean extends Model {
+public class TodoBean extends Model implements Serializable{
 
     @Column(name = "content")
     public String content;
