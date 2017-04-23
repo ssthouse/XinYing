b3e46babea57ef68fe5bf8980fa0b193c2d63198
ssthouse <ssthouse@163.com>
2017-04-23 09:27:08 +0800

    Add vue url config UI and function implemented

--- app/src/main/java/ssthouse/love/xinying/SettingActivity.java
+++ app/src/main/java/ssthouse/love/xinying/SettingActivity.java
@@ -2,12 +2,17 @@ package ssthouse.love.xinying;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import ssthouse.love.xinying.base.BaseActivity;
 import ssthouse.love.xinying.note.FastNoteConfigUtil;
+import ssthouse.love.xinying.utils.ToastUtil;
+import ssthouse.love.xinying.vue.VueConfig;
 
 /**
  * Created by ssthouse on 16/9/4.
@@ -20,6 +25,12 @@ public class SettingActivity extends BaseActivity {
     @Bind(R.id.id_cv_pull_my_note)
     CardView cvPullFastNote;
 
+    @Bind(R.id.id_et_vue_url)
+    EditText etVueUrl;
+
+    @Bind(R.id.id_tv_change_url)
+    TextView tvChangeUrl;
+
     @Override
     public void init() {
         initToolbar();
@@ -28,6 +39,19 @@ public class SettingActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 FastNoteConfigUtil.getInstance(SettingActivity.this).pullFastNoteFromCloud();
+            }
+        });
+
+        etVueUrl.setText(new VueConfig(this).getURL());
+
+        tvChangeUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(etVueUrl.getText().toString())) {
+                    return;
+                }
+                new VueConfig(SettingActivity.this).setUrl(etVueUrl.getText().toString());
+                ToastUtil.show(SettingActivity.this, "修改成功, 下次启动应用生效 :)");
             }
         });
     }
